437541327456007ee371a4bdf6fdb9eac96476e4
UdeepChowdary <udeepchowdary@gmail.com>
2025-05-27 21:32:57 +0530

    Remove multilingual support and clean up locale-related files

--- app/[locale]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-// This page redirects to the home page
-export default function Page({ params }: { params: { locale: string } }) {
-  redirect(`/${params.locale}/home`);
-}
